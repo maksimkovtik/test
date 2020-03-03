dfc44061534d53ad6bc2bc8299dbda19a34056b0
Maksim Kovtik <maksimkovtik@gmail.com>
2020-03-03 11:52:10 +0300

    fixed success message adaptiveness

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
 
+//регулярные выражения для проверки введённых данных
 var rv_password = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/
 var rv_email = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
 
+//Код анимации появления элементов формы
 $('.form_group').each(function(index, value){
    $(this).delay(index * 200).animate({opacity: 1}, 200);
 })
 
-//select list arrow styling
+//Скрипт поворота стрелочки в выпадающих списках
 $("select").click(function(e){
   $(this).focus();
   $(this).parent().toggleClass('rotate');
@@ -17,44 +19,41 @@ $("select").blur(function(){
   $(this).parent().removeClass('rotate');
 });
 
+
+//проверка формы и получение сообщений из файлов JSON
 $("#reg_form").submit(function(e){
     e.preventDefault();
     console.log("submit click");
     if(check_form())
     {
-        $.ajax({
-              url: 'server-ok.json',
-              type: 'post',
-              dataType: 'json',
-              data: JSON.stringify($("#reg_form").serialize()),
-              success: function(response){
-                console.log("ajax sending success");
-                $(this).reset();
-                reg_succes();
-                //console.log($("#reg_form").serialize());
-              },
-              error: function(response){
-                console.log("ajax sending error");
-                $('#complete').effect('shake',{ direction: "both", times: 3, distance: 2}, 200);
-                return false;
-              }
-       });
+      console.log("check_form() returns true");
+      $.getJSON('server-ok.json', function(json){
+        console.log("getting JSON OK message...");
+        console.log(json);   
+        $(this).trigger("reset");
+        reg_succes();
+      });
      }
     else
     {
-      console.log("check form error");
+      console.log("check_form() returns false")
       $('#complete').effect('shake',{ direction: "both", times: 3, distance: 2}, 200);
-      return false;
+      $.getJSON('server-error.json', function(json){
+        console.log("getting JSON ERROR message...");
+        console.log(json);
+      });
+      return;
     }
 })
 
+//при нажатии на кнопку, которая не имеет прямого отнощения к форме генерируем событие отправки формы
 $("#complete").click(function(e){ $("#reg_form").submit();});
-//end of select list arrow styling
 
 
+//функция проверяющая все поля формы перед отправкой
 function check_form(){
   console.clear();
-  console.log(JSON.stringify($("#reg_form").serialize()));
+  //console.log(JSON.stringify($("#reg_form").serialize()));
   if($("#fname").val() != '')
     {
       console.log("fname OK");
@@ -67,7 +66,7 @@ function check_form(){
             if($("#email").val() != '' && rv_email.test($("#email").val()))
             {
               console.log("email OK");
-              if($("#day").val() != '' && $("#month").val() != '' && $("#year").val() != '')
+              if($("#day select").val() != '' && $("#month select").val() != '' && $("#year select").val() != '')
               {
                   console.log("date OK");
                   if($("input[name=gender]:checked").val()){
@@ -86,17 +85,20 @@ function check_form(){
   return false;
 }
 
+//Функция показывающая сообщение об успещной решистрации
 function reg_succes(){
   console.log("function reg_success");
   $(".success_container").show().animate({opacity: 1}, 1500);
   $("svg").fadeOut(1500);
+  $(".form_container").fadeOut(1500).hide();
 }
 
 //start of blur event
+//Проверка поля после потери фокуса
 $('#email, #password, #cpassword').blur(function(e){
   var id = $(this).attr('id');
   var val = $(this).val();
-  console.log('blur id:' + id + "; val:" + val);
+  //console.log('blur id:' + id + "; val:" + val);
   switch(id)
   {
     case 'password': // one number, one capital letter, one lowercase letter, not less than 8 symbols
